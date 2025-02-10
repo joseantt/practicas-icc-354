a4c96ad0475f87ae79b2428fb6d3b437f5f1f5a1
Demian Reynoso <demianreynosogomez@gmail.com>
2025-02-09 23:58:53 -0400

    feat: project creation almost complete, missing how to retrieve current user.

--- practica-3/src/main/java/org/example/practica3/services/ProjectService.java
+++ practica-3/src/main/java/org/example/practica3/services/ProjectService.java
@@ -2,16 +2,27 @@ package org.example.practica3.services;
 
 import org.example.practica3.entities.Project;
 import org.example.practica3.repositories.ProjectRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class ProjectService {
-
     private final ProjectRepository projectRepository;
 
     public ProjectService(ProjectRepository projectRepository) {
         this.projectRepository = projectRepository;
     }
 
-    public Project fingByUserId(long id) {
-        return null;
+    public List<Project> findByUserId(long userId) {
+        return projectRepository.findByUserInfo_Id(userId);
+    }
+
+    public void deleteProject(Project project) {
+        projectRepository.delete(project);
+    }
+
+    public void saveProject(Project project) {
+        projectRepository.save(project);
     }
 }
