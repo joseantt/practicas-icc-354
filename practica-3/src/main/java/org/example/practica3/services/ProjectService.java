5c61ea896793ce9214255a488b22ffa7b87495e8
joseantt <joseant994@gmail.com>
2025-02-11 00:22:31 -0400

    fix: validations added, notification when created missing

--- practica-3/src/main/java/org/example/practica3/services/ProjectService.java
+++ practica-3/src/main/java/org/example/practica3/services/ProjectService.java
@@ -3,6 +3,13 @@ package org.example.practica3.services;
 import org.example.practica3.entities.Project;
 import org.example.practica3.repositories.ProjectRepository;
 
+<<<<<<< Updated upstream
+=======
+import java.util.List;
+import java.util.Optional;
+
+@Service
+>>>>>>> Stashed changes
 public class ProjectService {
 
     private final ProjectRepository projectRepository;
@@ -11,7 +18,24 @@ public class ProjectService {
         this.projectRepository = projectRepository;
     }
 
+<<<<<<< Updated upstream
     public Project fingByUserId(long id) {
         return null;
+=======
+    public Optional<Project> findByProjectId(long projectId) {
+        return projectRepository.findById(projectId);
+    }
+
+    public List<Project> findByUserId(long userId) {
+        return projectRepository.findByUserInfo_Id(userId);
+    }
+
+    public void deleteProject(Project project) {
+        projectRepository.delete(project);
+    }
+
+    public void saveProject(Project project) {
+        projectRepository.save(project);
+>>>>>>> Stashed changes
     }
 }
--- practica-3/src/main/java/org/example/practica3/views/MockupFormView.java
+++ practica-3/src/main/java/org/example/practica3/views/MockupFormView.java
@@ -14,6 +14,8 @@ import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.validator.RegexpValidator;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
@@ -21,19 +23,22 @@ import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.PermitAll;
 import org.example.practica3.entities.Header;
 import org.example.practica3.entities.Mockup;
+import org.example.practica3.entities.Project;
 import org.example.practica3.services.MockupService;
+import org.example.practica3.services.ProjectService;
 import org.springframework.http.HttpStatus;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 @PermitAll
-@Route(value = "projects/:projectId/create-mockup", layout = MainLayout.class)
+@Route(value = "project-management/:projectId/create-mockup", layout = MainLayout.class)
 @PageTitle("Create a Mockup | MockupAPP")
 public class MockupFormView extends VerticalLayout implements BeforeEnterObserver {
-    private Long projectId; // TODO: CHANGE WHEN IMPLEMENTING DEMIAN'S PART
+    private Long projectId;
 
     private final MockupService mockupService;
+    private final ProjectService projectService;
 
     private final FormLayout formLayout = new FormLayout();
     private final VerticalLayout headersContainer = new VerticalLayout();
@@ -50,13 +55,16 @@ public class MockupFormView extends VerticalLayout implements BeforeEnterObserve
     private final Button saveButton = new Button("Add this mockup");
     private final Button addHeaderButton = new Button("Add header");
     private final HorizontalLayout buttonLayout = new HorizontalLayout(addHeaderButton);
+    // Falta JWT
 
     private final Hr separator = new Hr();
     private final Hr separator2 = new Hr();
-    // Falta JWT
 
-    public MockupFormView(MockupService mockupService) {
+    private final Binder<Mockup> binder = new Binder<>(Mockup.class);
+
+    public MockupFormView(MockupService mockupService, ProjectService projectService) {
         this.mockupService = mockupService;
+        this.projectService = projectService;
 
         setupComponents();
         var title = new H3("Create a REST mockup");
@@ -66,6 +74,7 @@ public class MockupFormView extends VerticalLayout implements BeforeEnterObserve
                 responseTime, separator2, buttonLayout,
                 headersContainer, responseBody, expirationTime, saveButton);
 
+        createBinder();
         setFieldSizes();
         add(title, formLayout);
     }
@@ -81,7 +90,8 @@ public class MockupFormView extends VerticalLayout implements BeforeEnterObserve
         responseTime.setHelperText("If empty, the response will be immediate");
         expirationTime.setLabel("Expiration time");
 
-        path.setPlaceholder("/api/v1/example");
+        path.setPlaceholder("api/v1/example");
+        path.setHelperText("Path must not start with / and must not contain double slashes");
 
         accessMethod.setItems("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
         accessMethod.setValue("GET");
@@ -164,24 +174,21 @@ public class MockupFormView extends VerticalLayout implements BeforeEnterObserve
     // DATA
     private void saveMockup() {
         List<Header> headers = obtainHeaders();
-        Mockup mockup = Mockup.builder()
-                .name(name.getValue())
-                .description(description.getValue())
-                .path(path.getValue())
-                .accessMethod(accessMethod.getValue())
-                .responseCode(responseCode.getValue())
-                .contentType(contentType.getValue())
-                .body(responseBody.getValue())
-                .headers(headers)
-                .responseTimeInSecs(responseTime.isEmpty() ? 0 : responseTime.getValue())
-                .expirationTimeInHours(expirationTime.getValue())
-                .build();
-        // FALTA AGREGAR PROYECTO AL MOCKUP
+        Project project = projectService.findByProjectId(projectId).orElse(null);
+        Mockup mockup = new Mockup();
+        if(!binder.writeBeanIfValid(mockup) || project == null) {
+            return;
+        }
+
+        mockup.setProject(project);
+        mockup.setHeaders(headers);
+        mockup.setResponseTimeInSecs(responseTime.isEmpty() ? 0 : responseTime.getValue());
+
         for(Header header : headers) {
             header.setMockup(mockup);
         }
         mockupService.save(mockup);
-        UI.getCurrent().navigate(MainLayout.class); // TODO: CAMBIAR a vista de proyectos
+        UI.getCurrent().navigate(ProjectManagementView.class);
     }
 
     private List<Header> obtainHeaders() {
@@ -203,6 +210,34 @@ public class MockupFormView extends VerticalLayout implements BeforeEnterObserve
         return headers;
     }
 
+    private void createBinder() {
+        String pathRegex = "^(?!\\/|.*\\/\\/)(?:[A-Za-z0-9\\-._~!$&'()*+,;=:@]|%(?:[0-9A-Fa-f]{2})|\\/(?!\\/))*$";
+
+        binder.forField(name)
+                .asRequired("Name is required")
+                .bind(Mockup::getName, Mockup::setName);
+        binder.forField(description).bind(Mockup::getDescription, Mockup::setDescription);
+        binder.forField(path)
+                .asRequired("Path is required")
+                .withValidator(new RegexpValidator("Invalid path", pathRegex))
+                .bind(Mockup::getPath, Mockup::setPath);
+        binder.forField(accessMethod)
+                .asRequired()
+                .bind(Mockup::getAccessMethod, Mockup::setAccessMethod);
+        binder.forField(contentType)
+                .asRequired()
+                .bind(Mockup::getContentType, Mockup::setContentType);
+        binder.forField(responseCode)
+                .asRequired()
+                .bind(Mockup::getResponseCode, Mockup::setResponseCode);
+        binder.forField(expirationTime)
+                .asRequired()
+                .bind(Mockup::getExpirationTimeInHours, Mockup::setExpirationTimeInHours);
+        binder.forField(responseBody)
+                .asRequired("Response body is required")
+                .bind(Mockup::getBody, Mockup::setBody);
+    }
+
     @Override
     public void beforeEnter(BeforeEnterEvent event) {
         var projectIdParam = event.getRouteParameters().get("projectId");
@@ -212,6 +247,5 @@ public class MockupFormView extends VerticalLayout implements BeforeEnterObserve
         }
 
         projectId = Long.parseLong(projectIdParam.get());
-        // Esto se inicia luego del constructor
     }
 }
