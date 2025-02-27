acb375570db6bd7adb6c5fc745f37baab87f4de0
joseantt <joseant994@gmail.com>
2025-02-27 17:07:24 -0400

    feat: client implemented and server project created

---
+++ practica-5/endpoint-simulator/src/main/java/org/example/Main.java
@@ -0,0 +1,62 @@
+package org.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jms.Connection;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.Topic;
+import java.util.Random;
+
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+    private static Session session = null;
+    private static MessageProducer productor = null;
+    private static final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+    private static final int idDispositivo = new Random().nextInt(1000000);
+
+    public static void main(String[] args) {
+//        try{
+//            Connection connection = factory.createConnection("admin", "admin");
+//            connection.start();
+//            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+//            Topic topic = session.createTopic("sensores.topic");
+//            productor = session.createProducer(topic);
+//        } catch (Exception e) {
+//            logger.error("Error al crear la conexión", e);
+//            System.exit(1);
+//        }
+
+        while(true){
+            try{
+                Mensaje mensaje = sensorDetectaInformacion();
+                String json = objectMapper.writeValueAsString(mensaje);
+//                productor.send(session.createObjectMessage(json));
+                logger.info("Mensaje enviado -> {}", json);
+                Thread.sleep(60000);
+            } catch (Exception e) {
+                logger.error("Error al enviar el mensaje", e);
+            }
+        }
+    }
+
+    public static Mensaje sensorDetectaInformacion(){
+        Random random = new Random();
+
+        double mediaTemperatura = 27.5;
+        double desviacionEstandarTemperatura = 5.0;
+        double temperatura = mediaTemperatura + desviacionEstandarTemperatura * random.nextGaussian();
+
+        double mediaHumedad = 50.0;
+        double desviacionEstandarHumedad = 15.0;
+        double humedad = mediaHumedad + desviacionEstandarHumedad * random.nextGaussian();
+
+        return new Mensaje(temperatura, humedad, idDispositivo);
+    }
+}
